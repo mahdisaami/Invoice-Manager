bb5f8b27fe048b6babc8e881f037b7e895f930ef
Mahdi Saami <mahdisaami@gmail.com>
2020-11-24 12:54:53 +0330

    changing test

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,16 +1,24 @@
 from unittest import TestCase
 
-from app import app
+from app import app, db
+from models import User
 
 
 class TestAuthenticatedView(TestCase):
 
     def setUp(self):
         self.app = app.test_client()
+        self.user = User.create('info@gmail.com', '123')
+        db.session.add(self.user)
+        db.session.commit()
 
     def test_authenticated_users(self):
-        result1 = self.app.get('/render_sample/')
-        self.assertEqual(result1.status_code, 404)
+        user_data = {
+            'email': 'info@gmail.com',
+            'password': '123'
+        }
+        result1 = self.app.post('/login/', user_data, format='json' )
+        self.assertEqual(result1.status_code, 200)
 
         result2 = self.app.get('/invoice_template/')
-        self.assertEqual(result2.status_code, 302)
+        self.assertEqual(result2.status_code, 200)
